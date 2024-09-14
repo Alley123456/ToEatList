46aed332b2c969a4186487d6a3cb7af040bb25de
Alley123456 <3185574773@qq.com>
2024-09-14 19:28:12 +0800

    finish map

---
+++ src/components/map.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="demo-image">
+
+        <el-image style="width: 100px; height: 100px" :src="url" :fit="fill" />
+
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  
+  const url =
+    'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+  </script>
+  
+ 
