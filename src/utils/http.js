8e0870ef3b63f7bffacb8dd52637763557533716
Alley123456 <3185574773@qq.com>
2024-09-23 21:16:20 +0800

    🦄 refactor responder and interceptor

--- src/utils/http.js
+++ src/utils/http.js
@@ -7,21 +7,26 @@ timeout:5000
 })
 
 // 添加请求拦截器
-httpInstance.interceptors.request.use(function (config) {
-    // 在发送请求之前做些什么
-    return config;
-  }, function (error) {
-    // 对请求错误做些什么
-    return Promise.reject(error);
-  });
+httpInstance.interceptors.request.use(
+(config)=>{
+  return config;
+},
+(error)=>{
+  return Promise.reject(error);
+}
+
+
+);
 
 // 添加响应拦截器
-httpInstance.interceptors.response.use(res=>res.data,e=>{
-    ElMessage({
-        type:'warning',
-        message:e.response.data.message
-    })
-    return Promise.reject(e);
-  });
+httpInstance.interceptors.response.use(
+(response)=>{
+  return response.data;
+},
+(error)=>{
+  return Promise.reject(error)
+}
+
+);
 
   export default httpInstance
